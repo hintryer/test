7024b8edd9602640fd579d8747c6983eed0f19df
hintryer <gindeny+github@gmail.com>
2026-04-22 09:27:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,10 +10,12 @@ def get_ext_version_and_size(ext_id):
         print("版本号:", xml)
         # 👇 匹配：version="4.152.3"/>
         ver_match = re.search(r'version="(\d+)"/>', xml)
+        print("版本号:", ver_match)
         version = ver_match.group(1)
 
         # 👇 匹配 size
         size_match = re.search(r'size="(\d+)"', xml)
+        print("版本号:", size_match)
         size = size_match.group(1)
 
         return version, size
